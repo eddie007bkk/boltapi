e2076e6775325e4f9977f09f95a1629aa15d8d48
Louis To <Louis.To@Live.com>
2016-12-25 11:53:53 -0600

    Implemented simple web server

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"boltapi/boltdb"
 	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func init() {
@@ -19,4 +22,12 @@ func main() {
 	database.Put([]byte("bucket"), []byte("somekey"), []byte("somevalue"))
 	res, err := database.Get([]byte("bucket"), []byte("somekey"))
 	log.Println("Result: ", string(res), "Err:", err)
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Hello)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello User"))
 }
