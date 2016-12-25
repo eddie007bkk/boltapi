c25efc1bc3e72f8e23dbd90a6d70a184a9d122e2
Louis To <Louis.To@Live.com>
2016-12-25 14:24:54 -0600

    Start implementing request Handler, get & put

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"boltapi/boltdb"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -18,15 +19,35 @@ func main() {
 	dbfile := "/home/ubuntu/test.db"
 	database, _ = boltdb.NewDatabase(dbfile)
 
-	database.Put([]byte("bucket"), []byte("somekey"), []byte("somevalue"))
-
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/read/", read)
+	router.HandleFunc("/dbs/{db}/buckets/{bucket}/keys/{key}", reqHandler)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func read(w http.ResponseWriter, r *http.Request) {
-	log.Println(database)
+func reqHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "PUT":
+		put(w, r)
+	case "GET":
+		get(w, r)
+	}
+	vars := mux.Vars(r)
+	log.Println("db:", vars["db"])
+}
+
+func get(w http.ResponseWriter, r *http.Request) {
 	res, err := database.Get([]byte("bucket"), []byte("somekey"))
 	log.Println("Result: ", string(res), "Err:", err)
+}
+
+func put(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bucket := vars["bucket"]
+	keys := vars["key"]
+	vals, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(bucket, keys, vals)
+	database.Put([]byte("bucket"), []byte("somekey"), []byte("somevalue"))
 }
