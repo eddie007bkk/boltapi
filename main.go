df3eab74f608c37b10c4865ff6432ec15229a983
Louis To <Louis.To@Live.com>
2017-01-15 21:59:46 -0600

    Moved userRequest to requests.go

--- main.go
+++ main.go
@@ -9,32 +9,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type userRequest struct {
-	db, bucket, key, cmd string
-}
 
-func (userReq *userRequest) GetRequest(r *http.Request) {
-	vars := mux.Vars(r)
-	userReq.db = vars["db"]
-	userReq.bucket = vars["bucketName"]
-	userReq.key = vars["keyName"]
-	return
-}
-
-func (userReq *userRequest) GetUserDB(r *http.Request) {
-	if len(userReq.db) > 0 && (database == nil || (userReq.db != database.CurrentDB())) {
-		var err error
-		if database != nil {
-			err := database.DB.Close()
-			handleErr(err)
-		}
-		dbPath := dbsFolder + "/" + userReq.db
-		database, err = db.NewDatabase(dbPath)
-		handleErr(err)
-		return
-
-	}
-}
 
 func init() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
